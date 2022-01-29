e1cee689c8b64abc6665c596b502ee915b2a9464
Anna Watt <anna.watt@alfasystems.com>
2022-01-29 16:13:43 +0000

    Commit iniial external project

---
+++ anna.watt.external/src/test/java/com/oodlefinance/anna/watt/external/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oodlefinance.anna.watt.external;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
